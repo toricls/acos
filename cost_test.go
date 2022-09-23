068f6cbfe3a9b870de6db2cfdaba6da2d03a1530
Tori Hara <toriclstr@gmail.com>
2022-09-24 00:21:31 +0900

    test: add mocked test for GetCosts

--- cost_test.go
+++ cost_test.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"reflect"
 	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/costexplorer"
+	"github.com/aws/aws-sdk-go-v2/service/costexplorer/types"
 )
 
 func TestGetCosts(t *testing.T) {
@@ -27,6 +30,72 @@ func TestGetCosts(t *testing.T) {
 			},
 			want:    nil,
 			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetCosts(tt.args.ctx, tt.args.accounts, tt.args.opt)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetCosts() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetCosts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+type mockGetCostAndUsageAPI func(ctx context.Context, params *costexplorer.GetCostAndUsageInput, optFns ...func(*costexplorer.Options)) (*costexplorer.GetCostAndUsageOutput, error)
+
+func (m mockGetCostAndUsageAPI) GetCostAndUsage(ctx context.Context, params *costexplorer.GetCostAndUsageInput, optFns ...func(*costexplorer.Options)) (*costexplorer.GetCostAndUsageOutput, error) {
+	return m(ctx, params, optFns...)
+}
+
+func TestWithMock_GetCosts(t *testing.T) {
+	ceClient = mockGetCostAndUsageAPI(func(ctx context.Context, params *costexplorer.GetCostAndUsageInput, optFns ...func(*costexplorer.Options)) (*costexplorer.GetCostAndUsageOutput, error) {
+		t.Helper()
+		out := &costexplorer.GetCostAndUsageOutput{
+			ResultsByTime:            []types.ResultByTime{},
+			GroupDefinitions:         []types.GroupDefinition{},
+			DimensionValueAttributes: []types.DimensionValuesWithAttributes{},
+		}
+		return out, nil
+	})
+
+	type args struct {
+		ctx      context.Context
+		accounts Accounts
+		opt      AcosGetCostsOption
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Costs
+		wantErr bool
+	}{
+		{
+			name: "returns zero-cost information even GetCostAndUsage API returns no result", // Because newly created account has no billed cost in most cases.
+			args: args{
+				ctx: context.Background(),
+				accounts: Accounts{
+					"123456789012": Account{
+						Id:   toPointer("123456789012"),
+						Name: toPointer("test"),
+					},
+				},
+				opt: NewGetCostsOption(),
+			},
+			want: Costs{
+				"123456789012": Cost{
+					AccountID:               "123456789012",
+					AccountName:             "test",
+					LatestDailyCostIncrease: 0,
+					AmountLastMonth:         0,
+					AmountThisMonth:         0,
+				},
+			},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
